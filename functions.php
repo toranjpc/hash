87bca05a1a4466fd4a3332c123fd84f82552795a
nasser <naserkhanzadii@gmail.com>
2024-12-18 11:15:49 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,13 +7,17 @@ if (!function_exists('N_encode')) {
 
     function N_encode($plain_text, $key = 'N@SSEj', $iv_len = 16)
     {
-        // افزودن PKCS7 Padding
         $pad = 16 - (strlen($plain_text) % 16);
         $plain_text .= str_repeat(chr($pad), $pad);
 
         $iv = get_rnd_iv($iv_len);
-        $key = substr(hash('sha256', $key, true), 0, 32); // اطمینان از طول کلید 32 بایتی
+        $key = substr(hash('sha256', $key, true), 0, 32);
         $enc_text = openssl_encrypt($plain_text, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+
+        if ($enc_text === false) {
+            throw new Exception('Encryption failed');
+        }
+
         return base64_encode($iv . $enc_text);
     }
 }
@@ -24,27 +28,38 @@ if (!function_exists('N_decode')) {
         $enc_text = base64_decode($enc_text);
         $iv = substr($enc_text, 0, $iv_len);
         $enc_text = substr($enc_text, $iv_len);
-        $key = substr(hash('sha256', $key, true), 0, 32); // اطمینان از طول کلید 32 بایتی
+        $key = substr(hash('sha256', $key, true), 0, 32);
         $plain_text = openssl_decrypt($enc_text, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
 
-        // حذف PKCS7 Padding
+        if ($plain_text === false) {
+            throw new Exception('Decryption failed');
+        }
+
         $pad = ord($plain_text[strlen($plain_text) - 1]);
         return substr($plain_text, 0, -$pad);
     }
 }
 
 
-
 if (!function_exists('Base_url')) {
     function Base_url()
     {
-        $Base_url = $_SERVER['SERVER_NAME'];
-        $host = explode('.', $Base_url);
+        $serverName = filter_var($_SERVER['SERVER_NAME'], FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME);
+        if ($serverName === false) {
+            // پردازش خطا: دامنه نامعتبر
+            return null;
+        }
+
+        $host = explode('.', $serverName);
         $count = count($host);
-        if ($count > 2) $Base_url = $host[$count - 2] . '.' . $host[$count - 1];
-        return $Base_url;
+        if ($count > 2) {
+            $serverName = $host[$count - 2] . '.' . $host[$count - 1];
+        }
+        return $serverName;
     }
 }
+
+
 
 // $string="admin";
 // $ecodeed1 = N_encode($string);
