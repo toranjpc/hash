9a2f11fc8cadb4596a2f83ff6558223aaf14b05f
nasser <naserkhanzadii@gmail.com>
2025-03-08 02:14:56 +0330

    updated

--- functions.php
+++ functions.php
@@ -1,21 +1,19 @@
 <?php
 if (!function_exists('N_encode')) {
-    function get_rnd_iv($iv_len)
+    function N_encode($plain_text, $key = null, $iv_len = 16)
     {
-        return random_bytes($iv_len);
-    }
+        if (empty($iv_len)) $iv_len = 1;
+        $key = $key ?? random_bytes(32);
 
-    function N_encode($plain_text, $key = 'N@SSEj', $iv_len = 16)
-    {
         $pad = 16 - (strlen($plain_text) % 16);
         $plain_text .= str_repeat(chr($pad), $pad);
 
-        $iv = get_rnd_iv($iv_len);
+        $iv = random_bytes($iv_len);
         $key = substr(hash('sha256', $key, true), 0, 32);
-        $enc_text = openssl_encrypt($plain_text, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+        $enc_text = @openssl_encrypt($plain_text, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
 
         if ($enc_text === false) {
-            throw new Exception('Encryption failed');
+            return base64_decode(random_bytes(32));
         }
 
         return base64_encode($iv . $enc_text);
@@ -23,16 +21,25 @@ if (!function_exists('N_encode')) {
 }
 
 if (!function_exists('N_decode')) {
-    function N_decode($enc_text, $key = 'N@SSEj', $iv_len = 16)
+    function N_decode($enc_text, $key = null, $iv_len = 16)
     {
+        if (empty($iv_len)) $iv_len = 1;
+        if (!$key) {
+            $key = substr($enc_text, 0, 44);
+            $key = base64_decode($key);
+
+            $enc_text = substr($enc_text, 44);
+        } else {
+            $key = substr(hash('sha256', $key, true), 0, 32);
+        }
+
         $enc_text = base64_decode($enc_text);
         $iv = substr($enc_text, 0, $iv_len);
         $enc_text = substr($enc_text, $iv_len);
-        $key = substr(hash('sha256', $key, true), 0, 32);
-        $plain_text = openssl_decrypt($enc_text, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+        $plain_text = @openssl_decrypt($enc_text, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
 
         if ($plain_text === false) {
-            throw new Exception('Decryption failed');
+            return base64_decode(random_bytes(32));
         }
 
         $pad = ord($plain_text[strlen($plain_text) - 1]);
@@ -46,7 +53,6 @@ if (!function_exists('Base_url')) {
     {
         $serverName = filter_var($_SERVER['SERVER_NAME'], FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME);
         if ($serverName === false) {
-            // پردازش خطا: دامنه نامعتبر
             return null;
         }
 
@@ -61,9 +67,12 @@ if (!function_exists('Base_url')) {
 
 
 
-// $string="admin";
-// $ecodeed1 = N_encode($string);
-// $decodeed1 = N_decode($ecodeed1);
+$string = "admin";
+$ecodeed1 = N_encode($string, 'nasser', 16);
+$decodeed1 = N_decode($ecodeed1, 'nasser', 16);
 
-// echo "<br><br><br><br>کد شده <br>".$ecodeed1."<br> -> ".strlen($ecodeed1)."<br><br>";
-// echo "بازکردن: <br>".$decodeed1."<br> -> ".strlen($decodeed1)."<br>------------<br>";
+echo "کد شده <br>" . $ecodeed1;
+echo "<br>------------<br>بازکردن: <br>" . $decodeed1;
+
+// echo "کد شده <br>" . $ecodeed1 . "<br> -> " . strlen($ecodeed1);
+// echo "<br>------------<br>بازکردن: <br>" . $decodeed1 . "<br> -> " . strlen($decodeed1);
